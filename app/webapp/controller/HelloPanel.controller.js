191182c6a4f91560a82f76e0acee4e5ae4004ac2
은정 한 <hanjung25@naver.com>
2025-03-31 17:15:08 +0000

    서버에서 데이터 불러오기

--- app/webapp/controller/HelloPanel.controller.js
+++ app/webapp/controller/HelloPanel.controller.js
@@ -1,15 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment"
-], function (Controller, JSONModel, Fragment) {
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
         onInit: function () {
-            // Invoices.json 파일을 JSON 모델로 로드
-            var oModel = new JSONModel("/webapp/Invoices.json");
-            this.getView().setModel(oModel, "invoice");
+            var oModel = new JSONModel();
+
+            // 서버에서 데이터 가져오기
+            oModel.loadData("/odata/v4/request/Request") // 요청할 서버의 엔드포인트
+                .then(() => {
+                    console.log("서버에서 데이터를 성공적으로 가져왔습니다:", oModel.getData());
+                })
+                .catch((oError) => {
+                    console.error("데이터를 가져오는 데 실패했습니다:", oError);
+                });
+
+            // 가져온 데이터를 requestModel로 설정
+            this.getView().setModel(oModel, "requestModel");
         },
 
         getPage: function () {
