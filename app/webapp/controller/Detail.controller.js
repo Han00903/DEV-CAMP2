22d20dee6c1b4ee5cc91aea063138762b283ed73
은정 한 <hanjung25@naver.com>
2025-04-02 08:18:36 +0000

    상세 페이지 UI 완성

--- app/webapp/controller/Detail.controller.js
+++ app/webapp/controller/Detail.controller.js
@@ -1,18 +1,42 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.Detail", {
         onInit: function () {
             var oRouter = this.getOwnerComponent().getRouter();
-            oRouter.getRoute("detail").attachPatternMatched(this._onRouteMatched, this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
 
-        _onRouteMatched: function (oEvent) {
-            var sRequestNumber = oEvent.getParameter("arguments").request_number;
-            var oText = this.getView().byId("detailText");
-            oText.setText("Request Number: " + sRequestNumber);
+        _onObjectMatched: function (oEvent) {
+            var sProductName = oEvent.getParameter("arguments").product;
+            if (!sProductName) {
+                console.error("❌ 제품명이 없습니다.");
+                return;
+            }
+
+            var oModel = this.getView().getModel("products");
+            var oData = oModel.getProperty("/");
+
+            var oSelectedProduct = oData.find(item => item.Name === sProductName);
+            if (!oSelectedProduct) {
+                console.error("❌ 해당 제품 데이터를 찾을 수 없습니다.");
+                return;
+            }
+
+            var orequestModel = new JSONModel(oSelectedProduct);
+            this.getView().setModel(orequestModel, "requestModel");
+        },
+
+        handleClose: function () {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("overview");
+        },
+
+        toggleAreaPriority: function () {
+            console.log("✏️ 편집 기능 구현 예정");
         }
     });
 });
